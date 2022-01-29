a34f338138df0defe3c3c57988e7e2e4f600b671
liwi <77789571+dliwilb@users.noreply.github.com>
2022-01-29 20:15:26 +0100

    Update oasis-dates.js
    change page layout of get listed

--- script/oasis-dates.js
+++ script/oasis-dates.js
@@ -51,14 +51,11 @@ async function shakeIt() {
                 // document.getElementById('price-of-reaper').innerHTML = nftCurrentPrice;
         
                 document.getElementById('list-of-reapers').innerHTML += 
-                    '<p>Token ID: ' + tokenId + '<br> Seller: ' + seller + '<br> Order Hash: ' + orderHash + '<br>' +
-                    `<img src="${nftJSON.image}" width=300 height=300><br> Current Price: ` + 
-                    nftCurrentPriceBCH + ` BCH @ ${auctionType[orderInfo[0]]} | sold? ${orderInfo[10]} | cancelled? ${orderInfo[11]} | ` + 
-                    `<a href="https://oasis.cash/token/${nftContractAddress}/${tokenId}" target="_blank">check it out on OASIS&thinsp;<img src="icons8-external-link-16_goldish.png"></a> | <a href="${nftURI}" target="_blank">check attributes&thinsp;<img src="icons8-external-link-16_goldish.png"></a></p><br>`;
-                    // '<p>' + tokenId + '<br>' + seller + '<br>' + orderHash + '<br>' + nftCurrentPriceBCH + 
-                    // ` BCH ${auctionType[orderInfo[0]]} Sold? ${orderInfo[10]} Cancelled? ${orderInfo[11]}<br>` + 
-                    // nftURI + '<br>' + `<img src="${nftJSON.image}" width=300 height=300>` + '</p>';
-                    // `<p>Token ID: ${tokenId}<br> Seller: ${seller}<br> orderHash: ${orderHash}<br>`;
+                    `<span class="nftdisplay"><span class="nfttext">Reapers #${tokenId} listed for ${nftCurrentPriceBCH} BCH [${auctionType[orderInfo[0]]}]</span><a href="${nftJSON.image}" target="_blank"><img src="${nftJSON.image}" width=280 height=280></a><span class="nfttext">check it out on <a href="https://oasis.cash/token/${nftContractAddress}/${tokenId}" target="_blank">OASIS&thinsp;<img src="icons8-external-link-16_goldish.png"></a></span></span>`;
+                    // '<p>Token ID: ' + tokenId + '<br> Seller: ' + seller + '<br> Order Hash: ' + orderHash + '<br>' +
+                    // `<img src="${nftJSON.image}" width=300 height=300><br> Current Price: ` + 
+                    // nftCurrentPriceBCH + ` BCH @ ${auctionType[orderInfo[0]]} | sold? ${orderInfo[10]} | cancelled? ${orderInfo[11]} | ` + 
+                    // `<a href="https://oasis.cash/token/${nftContractAddress}/${tokenId}" target="_blank">check it out on OASIS&thinsp;<img src="icons8-external-link-16_goldish.png"></a> | <a href="${nftURI}" target="_blank">check attributes&thinsp;<img src="icons8-external-link-16_goldish.png"></a></p><br>`;
             }
 
         }
@@ -86,7 +83,7 @@ async function shakeIt() {
                     `<span class="nftdisplay"><span class="nfttext">Reapers #${tokenId} sold for ${nftSellPriceBCH} BCH</span><a href="${nftJSON.image}" target="_blank"><img src="${nftJSON.image}" width=280 height=280></a></span>`;
             }
         }
-        
+
         isRunning = false;
     }
 }
